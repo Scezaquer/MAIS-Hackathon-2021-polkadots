d0866e6951d02538ec5ae190a1faa69c0d0ac6f3
BÜCK-KAEFFER Aurélien <scezaquer@gmail.com>
2021-10-02 20:26:26 -0400

    Created a webapp

---
+++ Webapp/main.py
@@ -0,0 +1,11 @@
+#main.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Heyyyy!"
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
