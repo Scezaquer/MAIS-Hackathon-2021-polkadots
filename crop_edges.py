f54513ef3b36e92854871aeabf03116ff527807b
BÜCK-KAEFFER Aurélien <scezaquer@gmail.com>
2021-10-03 05:27:48 -0400

    Crop update
    Upgraded the accuracy of the crop, but its slower

--- crop_edges.py
+++ crop_edges.py
@@ -13,17 +13,29 @@ def remove_edges(path):
 
     #Creates an eliptical mask supposed to englobe the petri dish and remove everything outside centered around the center of mass
     dist_to_side = min(cx, cy, img.size[0]-cx, img.size[1]-cy)
-    boundingBox = [0, 0, 0, 0]
-    print(dist_to_side)
-    boundingBox[0] = round(cx-dist_to_side)
-    boundingBox[1] = round(cy-dist_to_side)
-    boundingBox[2] = round(cx+dist_to_side)
-    boundingBox[3] = round(cy+dist_to_side)
-    print(boundingBox)
+    for x in range(5):
+        
+        boundingBox = [0, 0, 0, 0]
+        print(dist_to_side)
+        boundingBox[0] = round(cx-dist_to_side)
+        boundingBox[1] = round(cy-dist_to_side)
+        boundingBox[2] = round(cx+dist_to_side)
+        boundingBox[3] = round(cy+dist_to_side)
+        print(boundingBox)
 
-    mask = Image.new('L', img.size, 0)
-    draw = ImageDraw.Draw(mask) 
-    draw.ellipse((0, 0) + img.size, fill=255)
+        mask = Image.new('L', img.size, 0)
+        draw = ImageDraw.Draw(mask) 
+        draw.ellipse(boundingBox, fill=255)
+
+        im2 = Image.new('L', img.size)
+        new_image = Image.composite(img, im2, mask)
+
+        if np.sum(np.array(new_image))/np.sum(np.array(img)) < 0.6:
+            dist_to_side += 0.1*(min(cx, cy, img.size[0]-cx, img.size[1]-cy) - dist_to_side)
+        elif 0.7 < np.sum(np.array(new_image))/np.sum(np.array(img)):
+            dist_to_side*=0.9
+        else:
+            break
 
     im2 = Image.new('RGB', img.size)
 
